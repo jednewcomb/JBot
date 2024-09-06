b4741ce614ad8573e371f71ea00d87cbbde244c5
Jed Newcomb <newcomb.jed@gmail.com>
2024-09-06 19:19:55 -0400

    mess around w/ commands

--- src/main/java/me/maktoba/listeners/SlashCommandListener.java
+++ src/main/java/me/maktoba/listeners/SlashCommandListener.java
@@ -6,16 +6,23 @@ import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+
+
 
 public class SlashCommandListener extends ListenerAdapter {
 
     @Override
     public void onGuildReady(GuildReadyEvent event) {
         List<CommandData> commandData = new ArrayList<>();
-        commandData.add(Commands.slash("say", "Bot will say what you tell it to."));
-        commandData.add(Commands.slash("sarcasm", "Every other character is uppercase."));
+        commandData.add(Commands.slash("say", "Bot will say what you tell it to")
+                   .addOption(OptionType.STRING, "content", "what bot says", true));
+
+        commandData.add(Commands.slash("sarcasm", "Every other character is uppercase"));
+        commandData.add(Commands.slash("spoiler", "Covers message with potential spoilers"));
+        commandData.add(Commands.slash("reverse", "Reverses given text"));
         event.getGuild().updateCommands().addCommands(commandData).queue();
     }
 
@@ -39,6 +46,20 @@ public class SlashCommandListener extends ListenerAdapter {
 
                 event.reply(toReturn.toString()).queue();
                 break;
+
+            case "spoiler":
+                String string = event.getOption("content", OptionMapping::getAsString);
+                event.reply("||" + string + "||").queue();
+
+            case "reverse":
+                String strung = event.getOption("content", OptionMapping::getAsString);
+
+                String out = "";
+                for (int i = strung.length() - 1 ; i >= 0; i--) {
+                    out += strung.charAt(i);
+                }
+
+                event.reply(out).queue();
 
             default:
                 event.reply("I can't handle that command right now :(").setEphemeral(true).queue();
