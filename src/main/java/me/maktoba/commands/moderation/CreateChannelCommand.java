26d09eec46a09f2654858a6f579d607ba2ec5824
Jed Newcomb <newcomb.jed@gmail.com>
2025-03-04 15:15:26 -0500

    Back in action! Fixing structure on CreateChannelCommand

--- src/main/java/me/maktoba/commands/moderation/CreateChannelCommand.java
+++ src/main/java/me/maktoba/commands/moderation/CreateChannelCommand.java
@@ -8,6 +8,8 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.entities.channel.concrete.Category;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
@@ -29,11 +31,26 @@ public class CreateChannelCommand extends Command {
                 .setChannelTypes(ChannelType.CATEGORY));
 
     }
+    //channelName, always will be there, user input.
+
+    //then we have channelType, also always will be there
+    // For types, we have:
+    // TEXT, VOICE, FORUM?, MEDIA?, NEWS, RULES, STAGE
+
+    //Channel Category, is optional. So we will have to cover when its there and when its not.
+
+    //When we have a channel category, that's where the new channel is going
+
+    //When we don't have a channel category, it goes into the default category(no named)
+
+
+    //The structure might be better if we have it like
+
+    // handleText, handleVoice, handleForum, handleAnnouncement, handleRules, handleStage
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
         //TODO: Once text and voice channels are taken care off, consider adding the others too
-
         Guild guild = event.getGuild();
         Member member = event.getMember();
         String memberName = Objects.requireNonNull(member).getEffectiveName();
@@ -42,76 +59,42 @@ public class CreateChannelCommand extends Command {
                 .getAsString()
                 .replace(" ", "-");
 
-        //theres gotta be a way to use the channel class here right? ... RIGHT?
         String desiredChannelType = Objects.requireNonNull(event.getOption("channeltype")).getAsString();
 
-        //if theres a category, put it in that category, otherwise put it in the default
-        if (event.getOption("category") != null) {
-            Channel ch = event.getOption("category").getAsChannel();
-            Category cg = event.getOption("category").getAsChannel().asCategory();
-            //idk why this wasn't working before but it is now,
-            //TODO: CG USE???
+        //if theres no category, lets put it in the default category
+        if (event.getOption("category") == null) {
+
+            //what channelType? - maybe getting the channel type should be its own function?
             if (desiredChannelType.equals("TEXT")) {
-                //this is making channels, its bad, fix it. two generals in text channels
-                if (!event.getOption("category").getAsChannel().asCategory().getChannels().toString().contains(desiredChannelType)) {
-                    guild.createTextChannel(desiredChannelName,
-                                    Objects.requireNonNull(event.getOption("category"))
-                                            .getAsChannel()
-                                            .asCategory())
-                            .queue();
-
-                    event.reply(desiredChannelName + " made in " + ch.getName()).queue();
-                } else {
-                    event.reply("A channel with that name exists in that category").queue();
-                }
-
-            } else {
-
-                if (!event.getOption("category").getAsChannel().asCategory().getChannels().toString().contains(desiredChannelType)) {
-                    guild.createVoiceChannel(desiredChannelName,
-                                    Objects.requireNonNull(event.getOption("category"))
-                                            .getAsChannel()
-                                            .asCategory())
-                            .queue();
-
-                    event.reply(desiredChannelName + " made in " + ch.getName()).queue();
-                } else {
-                    event.reply("A channel with that name exists in that category").queue();
-                }
-
+                handleText(guild, event, desiredChannelName, desiredChannelType);
             }
 
-        } else {
-
-            if (desiredChannelType.equals("TEXT")) {
-
-                //TODO: as of now, this isn't allowing creation of a guild in default if one of the categories
-                //TODO: has a guild with the same name as desiredChannelName, even if default doesn't have it
-                //TODO: might need some null checks on the channels before we loop through them
-                //TODO: if get channels is null, we have nothing to worry about, create the channel
-
-                if (existsInDefault(guild, desiredChannelName)) {
-                    event.reply(desiredChannelName + " already exists in the default category").queue();
-                    return;
-                }
-                guild.createTextChannel(desiredChannelName).queue();
-                event.reply(desiredChannelName + " created").queue();
-
-            } else {
-
-                if (existsInDefault(guild, desiredChannelName)) {
-                    event.reply(desiredChannelName + " already exists in the default category").queue();
-                    return;
-                }
-                guild.createVoiceChannel(desiredChannelName).queue();
-                event.reply(desiredChannelName + " created").queue();
-
-            }
         }
 
+        Channel ch = event.getOption("category").getAsChannel();
+        Category category = event.getOption("category").getAsChannel().asCategory();
+
+        if (desiredChannelType.equals("TEXT")) {
+            handleText(guild, event, desiredChannelName, desiredChannelType, category);//LEFT OFF HERE
+        }
     }
 
-    public boolean existsInDefault(Guild guild, String chanName) {
+    public void handleText(Guild guild, SlashCommandInteractionEvent event, String channelName, String channelType) {
+        if (nameExistsInDefault(guild, channelName)) {
+            event.reply("There is already a channel with that name in the default category.").queue();
+            return;
+        }
+
+        guild.createTextChannel("general").queue();
+        event.reply(channelName + " created in the default category.").queue();
+    }
+
+    //when we have the category
+    public void handleText(Guild guild, SlashCommandInteractionEvent event, String channelName, String channelType, Category category) {
+        //YOU LEFT OFF HERE
+    }
+
+    public boolean nameExistsInDefault(Guild guild, String chanName) {
         for (Channel ch : guild.getChannels()) {
             //if its of type category, it must be a channel
             if (ch.getType() == ChannelType.CATEGORY) {
@@ -124,5 +107,9 @@ public class CreateChannelCommand extends Command {
         }
 
         return false;
+    }
+
+    public boolean nameExistsInCategory(Guild guild, String chanName) {
+
     }
 }
